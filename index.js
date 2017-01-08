3c3061b8a24ebd8f9de158608a70e1e7dc59eaf8
Seck Wei <seckwei93@gmail.com>
2017-01-08 18:55:33 +0000

    Configuring access control origins - temporarily allowing all origins to access this API

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = require('express')(),
     port = process.env.PORT || 8888, 
     tcom = require('thesaurus-com');
 
+app.use((req, res, next) => {
+    // Temporarily allow all origins
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
 app.get('/q', (req, res) => {
     if(req.query.word){
         res.json(tcom.search(req.query.word).synonyms);
